40f94cf9eb5eb60065f6952392399778d19cf2db
Ahmad Kadado <akadado@gmail.com>
2024-07-30 15:50:30 -0500

    added more functions and tests and one failed test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 function sum(a, b) {
     return a + b;
-  }
+}
   
-  module.exports = sum;
+function subtract(a, b) {
+    return a - b;
+}
+  
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = {
+    sum,
+    subtract,
+    multiply,
+};
   
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,3 +3,15 @@ const sum = require('../src/index');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('subtracts 5 - 4 to equal 1', () => {
+    expect(sum.subtract(5, 4)).toBe(1);
+});
+
+test('multiplies 5 * 5 to equal 25', () => {
+expect(sum(5, 5)).toBe(25);
+});
+
+test('adds 1 + 2 to equal 3', () => {
+expect(sum(1, 2)).toBe(4);
+});
