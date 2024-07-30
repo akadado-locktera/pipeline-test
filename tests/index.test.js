adf64e28b1009328cb33a74cd32a8ed82c0d9e22
Ahmad Kadado <akadado@gmail.com>
2024-07-30 16:41:49 -0500

    test ruleset

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,11 +5,11 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('subtracts 5 - 4 to equal 1', () => {
-    expect(sum.subtract(5, 4)).toBe(1);
+    expect(subtract(5, 4)).toBe(1);
 });
 
 test('multiplies 5 * 5 to equal 25', () => {
-expect(sum(5, 5)).toBe(25);
+expect(multiply(5, 5)).toBe(25);
 });
 
 test('adds 1 + 2 to equal 3', () => {
